b05dc25fe00b64dad9369ced9fa81aefddbea0cd
yui masuko <yui.t.tom@gmail.com>
2021-08-09 23:30:02 +0900

    タスクモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_140731) do
+ActiveRecord::Schema.define(version: 2021_08_09_142002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
+    t.string "title", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
