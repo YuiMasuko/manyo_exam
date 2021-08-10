40b92765c711186bfe4fed4ae4db95c13716efd9
yui masuko <yui.t.tom@gmail.com>
2021-08-10 11:12:07 +0900

    新規作成・編集画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,29 @@
 class TasksController < ApplicationController
+
   def index
-    @tasks = Tasks.all
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
+    if params[:back]
+      @task = Task.new(task_params)
+    else
+      @task= Task.new
+    end
   end
 
   def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice: "登録しました！"
+      else
+        render :new
+      end
+    end
   end
 
   def show
@@ -15,4 +31,10 @@ class TasksController < ApplicationController
 
   def edit
   end
+
+  private
+  def task_params
+    params.require(:task).permit(:task,:title,:content)
+  end
+
 end
