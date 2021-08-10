c8fce6c97e3b311a56d0ae236ad118d6be77d2e0
yui masuko <yui.t.tom@gmail.com>
2021-08-10 11:31:40 +0900

    一覧・詳細・編集画面の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit update ]
 
   def index
     @tasks = Task.all
@@ -32,7 +33,18 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def update
+    if @task.update(task_params)
+      redirect_to new_task_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
   def task_params
     params.require(:task).permit(:task,:title,:content)
   end
