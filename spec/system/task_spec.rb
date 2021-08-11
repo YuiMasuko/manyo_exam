0894db827260639352b41729c3f419f82a195885
yui masuko <yui.t.tom@gmail.com>
2021-08-11 11:50:49 +0900

    共通化

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task) }
+  let!(:second_task) { FactoryBot.create(:second_task) }
   before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
+    visit tasks_path
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -19,9 +20,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
-        visit tasks_path
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'test_title'
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
@@ -33,12 +32,12 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
         visit task_path(id: task.id)
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'test_title'
       end
     end
   end
