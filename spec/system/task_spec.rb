993c2a2faa1ae7f44922f895b9d3ddedb1a9d52f
yui masuko <yui.t.tom@gmail.com>
2021-08-14 11:42:15 +0900

    ユーザーモデルとシード

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
       visit new_task_path
-      fill_in "task[title]", with: "test_title"
-      fill_in "task[content]", with: "test_content"
+      fill_in "task[title]", with: 'test_title'
+      fill_in "task[content]", with: 'test_content'
       fill_in 'task[expired_at]', with: '2021-08-11 00:00:00'.to_date
       find('.field_status').set(1)
-      click_on "submit"
-      expect(page).to have_content "test_title"
-      expect(page).to have_content "test_content"
-      expect(page).to have_content "未着手"
+      click_on '送信'
+      expect(page).to have_content 'test_title'
+      expect(page).to have_content 'test_content'
+      expect(page).to have_content '未着手'
       end
     end
   end
